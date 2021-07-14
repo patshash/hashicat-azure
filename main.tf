d7da44dfe9c18b644568a24abfb0d2282f836079
Patrick <82187582+patshash@users.noreply.github.com>
2021-07-14 15:41:31 +1000

    remove ssh access

--- main.tf
+++ main.tf
@@ -63,18 +63,6 @@ resource "azurerm_network_security_group" "catapp-sg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 101
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
 
 resource "azurerm_network_interface" "catapp-nic" {
